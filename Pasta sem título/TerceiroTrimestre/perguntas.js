b93ddb8a1b3e31d6fb4def33d973bffe97ca9e4a
Lebron James <145369478+gabexs@users.noreply.github.com>
2024-10-30 14:51:50 +0000

    Add files via upload

---
+++ Pasta sem título/TerceiroTrimestre/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'O Corinthians vai cair esse ano?',
+    'SIMM'
+)
+
+criaCartao(
+    'NBA',
+    'Qual a maior franquia da NBA?',
+    'Boston Celtics'
+)
+
+criaCartao(
+    'Bola de ouro',
+    'Quem devia levar a bola de ouro em 2024?',
+    'Vini Jr.'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual o maior clube do Mundo?',
+    'Real Madrid'
+)
